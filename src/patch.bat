6e35da33fd558784610c24c2657764d78ee34c9e
Tabitha Hanegan <tabithahanegan@gmail.com>
2025-10-19 14:01:06 -0400

    der

--- src/patch.bat
+++ src/patch.bat
@@ -1,7 +1,7 @@
 @echo off
 title Mario Party 6 - DX Patcher
 
-set VERSION=25.10.18
+set VERSION=25.10.19
 
 echo = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
 echo Mario Party 6 - DX Patcher!
